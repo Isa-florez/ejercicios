b303bcd6fd50b784baa0d2bc91cc7efad0ebbacc
Isabella Flórez <isa11cano28@gmail.com>
2025-11-12 18:32:13 -0500

    Delete gasolinera2.py

--- gasolinera2.py
+++
@@ -1,9 +0,0 @@
-totalLitros = 0 # acumulador para los litros vendidos
-while totalLitros <= 100:
-    venta = float(input("ingrese los litrosvendidos: "))
-    totalLitros += venta 
-
-    if totalLitros < 100:
-        print("sigue vendiendo")
-    else:
-        print("meta diaria alcanzada")
